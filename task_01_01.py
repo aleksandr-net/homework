9594895c6dfd3a82bf1adcadb7f18985cbbde590
Aleksandr Net <aleksandr.oliferuk@gmail.com>
2017-09-30 23:31:41 +0300

    Подтюнил

--- task_01_01.py
+++ task_01_01.py
@@ -6,10 +6,10 @@ if ploshad_uchastka_v_sotkah < 0:
     print('Введены некорректные данные!')
 else:
     ploshad_1_gryadki = 15 * 25
-    print('Площадь 1й грядки = ', ploshad_1_gryadki, ' квадратных метров')
+    print('Площадь 1й грядки =', ploshad_1_gryadki, ' квадратных метров')
     ploshad_uchastka_v_mkv = ploshad_uchastka_v_sotkah * 100
-    print('Площадь учатска = ', ploshad_uchastka_v_mkv, 'квадратных метров')
+    print('Площадь учатска =', ploshad_uchastka_v_mkv, 'квадратных метров')
     gryadok_na_uchastke = int(ploshad_uchastka_v_mkv / ploshad_1_gryadki)
-    print('На участке поместится грядок: ', gryadok_na_uchastke)
+    print('На участке поместится грядок:', gryadok_na_uchastke)
     neispolzovannie_metry = ploshad_uchastka_v_mkv - ploshad_1_gryadki * gryadok_na_uchastke
-    print('Осталось не занято ', int(neispolzovannie_metry), 'квадратных метров')
+    print('Осталось не занято', int(neispolzovannie_metry), 'квадратных метров')
