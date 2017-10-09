84456b8679504ce3fb50e2be59b0d96aa8823c6a
Aleksandr Net <aleksandr.oliferuk@gmail.com>
2017-10-09 20:24:56 +0300

    Исправил задание 3-1

--- task_03_01.py
+++ task_03_01.py
@@ -2,7 +2,7 @@
 
 import datetime
 
-def get_days_to_new_year:
+def get_days_to_new_year():
     now = datetime.datetime.now()
     next_year = datetime.datetime(now.year + 1, 1, 1)
     do_novogo_goda = next_year - now
