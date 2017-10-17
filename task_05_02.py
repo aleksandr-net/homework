9cf883d53889948eb492ba0ace64a3ba85b2da75
Aleksandr Oliferuk <aleksandr.oliferuk@gmail.com>
2017-10-17 13:33:13 +0300

    Задача 5-2

--- task_05_02.py
+++ task_05_02.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3.5
-def zero_check(x, y):
-    if x == 0 or y == 0:
-        raise ValueError
-    else:
-        pass
-
-
 def get_quadrant_number(x, y):
     try:
-        x = float(x)
-        y = float(y)
 
-        zero_check(x, y)
+        x, y = float(x), float(y)
+
+        if 0 in (x, y):
+            raise ValueError
 
         if (x > 0) and (y > 0):
             return 1
