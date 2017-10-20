41c1028a611921a47bbda29e5d10110aee576bd3
Aleksandr Oliferuk <aleksandr.oliferuk@gmail.com>
2017-10-20 12:58:41 +0300

    Задача 7-3. Не работает двойное декорирование =(

--- task_07_03.py
+++ task_07_03.py
@@ -4,32 +4,61 @@ from functools import wraps
 
 
 def strict_argument_types(func):
-    @wraps(func)
+    # @wraps(func)
+    global args
+
     def wrapper(*args):
         call_args = getcallargs(func, *args)
+        result = func(*args)
+        print(call_args)
 
         for key, val in call_args.items():
+            print("key = {}, val = {}".format(key, val))
             sig = signature(func).parameters[key].annotation
+
             try:
                 if sig != type(val):
                     raise TypeError
             except TypeError:
                 raise TypeError("The argument {} must be {}, passed {}".format(key, sig, type(val)))
-        return func(*args)
+
+        return result
+
     return wrapper
 
 
 def strict_return_type(func):
-    @wraps(func)
+    # @wraps(func)
+    global args
+
     def wrapper(*args):
+        global result
+        global sig
+        result = func(*args)
+        sig = signature(func)
+        # print(sig)
+
         try:
-            sig = signature(func)
-            result = func(*args)
             if sig.return_annotation != type(result):
                 raise TypeError
             else:
                 pass
         except TypeError:
             raise TypeError("The return value must be {}, not {}".format(sig.return_annotation, type(result)))
+
         return result
+
     return wrapper
+
+
+result = 0
+sig = signature(strict_return_type)
+
+
+@strict_argument_types
+@strict_return_type
+def summa(a: int, b: int, c: int) -> int:
+    return a + b + c
+
+
+print(summa(2, 2, 2))
