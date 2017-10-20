81fbfd935488ee444a106e4da7a4b45c125150d3
Aleksandr Oliferuk <aleksandr.oliferuk@gmail.com>
2017-10-20 17:38:08 +0300

    Задача 7-3

--- task_07_03.py
+++ task_07_03.py
@@ -1,64 +1,38 @@
 #!/usr/bin/python3.5
-from inspect import signature, getcallargs
+from inspect import signature
 from functools import wraps
 
 
 def strict_argument_types(func):
-    # @wraps(func)
-    global args
-
+    @wraps(func)
     def wrapper(*args):
-        call_args = getcallargs(func, *args)
-        result = func(*args)
-        print(call_args)
+        sig = signature(func)
+        bind_args = sig.bind(*args).arguments
 
-        for key, val in call_args.items():
-            print("key = {}, val = {}".format(key, val))
-            sig = signature(func).parameters[key].annotation
+        for key, val in bind_args.items():
+            anno = sig.parameters[key].annotation
+            if not isinstance(val, anno):
+                raise TypeError('The argument "{}" must be "{}", passed "{}"'.format(key, anno, type(val)))
+            else:
+                pass
 
-            try:
-                if sig != type(val):
-                    raise TypeError
-            except TypeError:
-                raise TypeError("The argument {} must be {}, passed {}".format(key, sig, type(val)))
-
-        return result
+        return func(*args)
 
     return wrapper
 
 
 def strict_return_type(func):
-    # @wraps(func)
-    global args
-
+    @wraps(func)
     def wrapper(*args):
-        global result
-        global sig
         result = func(*args)
         sig = signature(func)
-        # print(sig)
+        res_type = type(result)
 
-        try:
-            if sig.return_annotation != type(result):
-                raise TypeError
-            else:
-                pass
-        except TypeError:
-            raise TypeError("The return value must be {}, not {}".format(sig.return_annotation, type(result)))
+        if not isinstance(result, sig.return_annotation):
+            raise TypeError('The return value must be "{}", not "{}"'.format(sig.return_annotation, res_type))
+        else:
+            pass
 
         return result
 
     return wrapper
-
-
-result = 0
-sig = signature(strict_return_type)
-
-
-@strict_argument_types
-@strict_return_type
-def summa(a: int, b: int, c: int) -> int:
-    return a + b + c
-
-
-print(summa(2, 2, 2))
