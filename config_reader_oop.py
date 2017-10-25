6e0f1c838c6a844fd89a6d4c3143e0334b41ea66
Aleksandr Oliferuk <aleksandr.oliferuk@gmail.com>
2017-10-25 17:35:25 +0300

    Задача на проверку формата файлов. ООП

---
+++ config_reader_oop.py
@@ -0,0 +1,23 @@
+class XmlConfigReader(object):
+    def __init__(self, source):
+        pass
+
+class CsvComfigReader(object):
+    def __init__(self, source):
+        pass
+
+class CustonConfigReader(object):
+    def __init__(self, source):
+        pass
+
+class XmlConfigWriter(object):
+    def __init__(self, source):
+        pass
+
+class CsvConfigWriter(object):
+    def __init__(self, source):
+        pass
+
+class CustomConfigWriter(object):
+    def __init__(self, source):
+        pass
